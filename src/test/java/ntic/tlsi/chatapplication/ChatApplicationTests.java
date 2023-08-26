e193302e696373be8d458ba93eb5786417345560
Mossab dehane <mossabdehane19@gmail.com>
2023-08-26 23:10:38 +0200

    first commit config package

---
+++ src/test/java/ntic/tlsi/chatapplication/ChatApplicationTests.java
@@ -0,0 +1,13 @@
+package ntic.tlsi.chatapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
